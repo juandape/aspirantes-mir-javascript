cb325bda472a959593a62faf6ebfc83983128c5f
Juan David Peña <juandavidp76@hotmail.com>
2023-03-10 18:30:26 -0500

    ejercicio 1

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola(str){
+    return `Hola ${str}!`
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
